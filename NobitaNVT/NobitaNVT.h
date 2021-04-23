d8256a3b926fa58b046b37eb68cbe1730d288f46
Tinh <nguuyenvantinhfithou@gmail.com>
2021-04-23 15:45:14 +0700

    Initial Commit

---
+++ NobitaNVT/NobitaNVT.h
@@ -0,0 +1,19 @@
+//
+//  NobitaNVT.h
+//  NobitaNVT
+//
+//  Created by Nguyen Van Tinh on 4/23/21.
+//  Copyright © 2021 Nguyen Van Tinh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NobitaNVT.
+FOUNDATION_EXPORT double NobitaNVTVersionNumber;
+
+//! Project version string for NobitaNVT.
+FOUNDATION_EXPORT const unsigned char NobitaNVTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NobitaNVT/PublicHeader.h>
+
+
---
+++ NobitaNVT/NobitaNVT.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  NobitaNVT
+//
+//  Created by Nguyen Van Tinh on 4/23/21.
+//  Copyright © 2021 Nguyen Van Tinh. All rights reserved.
+//
+
+import Foundation
+public struct NobitaNVT{
+    public static func showME(){
+        print("Show me")
+    }
+}
